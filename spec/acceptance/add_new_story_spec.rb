c001f4bb6a40f61aac51bfc683e050c1ac699be2
Alan Gardner <alan@armoin.com>
2014-06-22 15:16:45 +0100

    Test refactor
    Signed-off-by: Alan Gardner <alan@armoin.com>

--- spec/acceptance/add_new_story_spec.rb
+++ spec/acceptance/add_new_story_spec.rb
@@ -3,12 +3,28 @@ require 'spec_helper'
 describe 'Reading page' do
   before :each do
     visit '/stories/new'
+
+    expect(Story.count).to eql(0)
+
+    fill_in 'story[title]', :with => 'My new story'
+    click_button('Add')
+  end
+
+  after :each do
+    Story.destroy_all
   end
 
   it "adds a new story" do
-    expect(Story.count).to eql(0)
-    fill_in 'story[title]', :with => 'My new story'
-    click_button('Add')
     expect(Story.count).to eql(1)
+  end
+
+  context 'new story' do
+    before :each do
+      @story = Story.first
+    end
+
+    it "has a title" do
+      expect(@story.title).to eql('My new story')
+    end
   end
 end
